af3696bf2059dc7203692348127004d6ee748187
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-04-08 03:37:15 -0400

    use Game component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Game from './components/Game';
+
+const minutesToAdd = 1;
+const currentTime = new Date().getTime();
+const futureTime = new Date(currentTime + minutesToAdd*60000);
+
+
 function App() {
+  const calculateTimeLeft = (difference) => {
+    let timeLeft = {
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(futureTime - currentTime);
+
+  useEffect(() => {
+    const difference = futureTime - new Date();
+    if (difference > 0) {
+      setTimeout(() => {
+        setTimeLeft(calculateTimeLeft(difference));
+      }, 1000);
+    }
+  });
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +45,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Game loggedUser={{ username: "James"}} word={"Word"} currentPhase={"Voting"} timeLeft={timeLeft} />
     </div>
   );
 }
